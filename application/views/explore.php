9ff2e5a60fee706c1381e747ac0ecfca3d088e42
mattellenburg <mattellenburg@gmail.com>
2015-10-18 11:35:53 -0400

    Explore UA
    minor explore UI updates

--- application/views/explore.php
+++ application/views/explore.php
@@ -6,14 +6,37 @@
     <div id="instructions">
         <h2>Explore</h2>
         <p>Click on the map to submit a new location. Click on a point to get more information about that location.</p>
-    </div>
-    <div id="googleMap">
+        <div id="googleMap">
+        </div>
     </div>
     <div id="pointInformation">
         <h2>Information</h2>
     </div>
     <div id="filters">
         <h2>Filters</h2>
+        <form action="<?= base_url('index.php/explore/index') ?>">
+            <p>
+                Average Rating: 
+                <input type="radio" name="rating" value="1" />
+                <input type="radio" name="rating" value="2" />
+                <input type="radio" name="rating" value="3" />
+                <input type="radio" name="rating" value="4" />
+                <input type="radio" name="rating" value="5" />
+                or higher
+            </p>
+            <p>Submitted by Me: <input type="checkbox" name="mysubmissions"></p>
+            <p>Approval Status: <input type="radio" name="approvalstatus" value="0">All<input type="radio" name="approvalstatus" value="1">Approved<input type="radio" name="approvalstatus" value="2">Pending</p>
+            <p>
+                Keyword Search: 
+                <div id="keywords">
+                    <?php foreach ($keywords as $keyword): ?>
+                        <input name="keywords[]" type="checkbox" value=" <?php echo $keyword->id ?> " /><?php echo $keyword->keyword ?><br>
+                    <?php endforeach; ?>
+                </div>
+            </p>
+            <p>Title/Description Search: <input type="text" name="search" /></p>
+            <p><input type="submit" value="Search" /></p>
+        </form>
     </div>
 </div>
 
@@ -118,19 +141,18 @@ function loadPoints(map, sessionid) {
                 infoWindow.setContent('<b>Average Rating:</b> ' + parseFloat(Math.round(locations[i][8])).toFixed(1));
                 infoWindow.open(map, marker);
 
-                document.getElementById('pointInformation').innerHTML = '<h3>' + locations[i][0] + '</h3>';
-                document.getElementById('pointInformation').innerHTML = document.getElementById('pointInformation').innerHTML + '<p>' + locations[i][3] + '</p>';
+                document.getElementById('pointInformation').innerHTML = '<h2>Information</h2>';
+                document.getElementById('pointInformation').innerHTML += '<h3>' + locations[i][0] + '</h3>';
+                document.getElementById('pointInformation').innerHTML += '<p>' + locations[i][3] + '</p>';
 
                 if (locations[i][5] == 'confirmed') {
-                    document.getElementById('pointInformation').innerHTML = document.getElementById('pointInformation').innerHTML;
-
                     if (sessionid>0) {
-                        document.getElementById('pointInformation').innerHTML = document.getElementById('pointInformation').innerHTML + '<p>Your Rating: ' + locations[i][7] + '</p>';
+                        document.getElementById('pointInformation').innerHTML += '<p>Your Rating: ' + locations[i][7] + '</p>';
 
                         var ratingsystem = '<p>Rate this location</p>';
                         ratingsystem += '<form action=' + "<?= base_url('index.php/explore/index') ?>" + '/' + locations[i][6].trim() + '><input name="rating" type="radio" value=1 class="star"/> <input name="rating" type="radio" value=2 class="star"/> <input name="rating" type="radio" value=3 class="star"/> <input name="rating" type="radio" value=4 class="star"/> <input name="rating" type="radio" value=5 class="star"/><input type=submit value=Rate></form>';
 
-                        document.getElementById('pointInformation').innerHTML = document.getElementById('pointInformation').innerHTML + ratingsystem;
+                        document.getElementById('pointInformation').innerHTML += ratingsystem;
 
                         var keywordlist = '<div id="keywords">';
                         <?php foreach ($keywords as $keyword): ?>
