15c91c4c79cf9f99dcc43fb7f996bc4d4b4a0bda
mattellenburg <mattellenburg@gmail.com>
2015-11-12 20:35:17 -0500

    Garmin
    Added garmin activity page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,6 +8,7 @@ class Admin extends CI_Controller {
         $this->load->helper(array('url'));
         $this->load->model('point_model');
         $this->load->model('point_pending_model');
+        $this->load->library('table');
     }
 
     public function index() {
@@ -24,6 +25,30 @@ class Admin extends CI_Controller {
         }
     }
 
+    public function garmin($start = 1) {
+        $data = new stdClass();
+        $data->message = '';
+        $data->start = $start;
+
+        $json_data = json_decode(file_get_contents('http://connect.garmin.com/proxy/activitylist-service/activities/mattellenburg?start=1&limit=10000'), true);
+        $activitylist = $json_data['activityList'];
+        $data->activities = sizeof($activitylist);
+        $this->table->set_heading('ID', 'Name', 'Description', 'Date', 'Activity Type', 'Event Type', 'Distance', 'Duration', 'Calories', 'Avg HR', 'Max HR');
+
+        for($i=($start*10)-10; $i<($start*10); $i++) {
+            $this->table->add_row($activitylist[$i]['activityId'], $activitylist[$i]['activityName'], $activitylist[$i]['description'], date('m-d-Y', strtotime($activitylist[$i]['startTimeLocal'])), $activitylist[$i]['activityType']['typeKey'], $activitylist[$i]['eventType']['typeKey'], number_format(floatval($activitylist[$i]['distance'])*0.00062137, 2), gmdate("H:i:s", $activitylist[$i]['duration']), number_format($activitylist[$i]['calories'], 0), $activitylist[$i]['averageHR'], $activitylist[$i]['maxHR']);
+        }
+
+        if ($_SESSION['is_admin']) {
+            $this->load->view('header', $data);
+            $this->load->view('admin/garmin', $data);
+            $this->load->view('footer');
+        }
+        else {
+            redirect('/');
+        }
+    }
+    
     public function approvepoints($id = NULL) {
         $data = new stdClass();
         $data->message = '';
---
+++ application/views/admin/garmin.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php
+$tmpl = array (
+    'table_open'          => '<table border="1" cellpadding="2" cellspacing="0">',
+
+    'heading_row_start'   => '<tr>',
+    'heading_row_end'     => '</tr>',
+    'heading_cell_start'  => '<th>',
+    'heading_cell_end'    => '</th>',
+
+    'row_start'           => '<tr>',
+    'row_end'             => '</tr>',
+    'cell_start'          => '<td>',
+    'cell_end'            => '</td>',
+
+    'row_alt_start'       => '<tr>',
+    'row_alt_end'         => '</tr>',
+    'cell_alt_start'      => '<td>',
+    'cell_alt_end'        => '</td>',
+
+    'table_close'         => '</table>'
+);
+
+$this->table->set_template($tmpl);
+
+echo $this->table->generate();          
+?>
+<a href="<?= base_url('index.php/admin/garmin/1')?>"><<</a>&nbsp;&nbsp;&nbsp;<a href="<?= base_url('index.php/admin/garmin/'.floor($start-1))?>"><</a>&nbsp;&nbsp;&nbsp;Activities <?= $start*10 - 9 ?> - <?= $start*10 ?> of <?= $activities?>&nbsp;&nbsp;&nbsp;<a href="<?= base_url('index.php/admin/garmin/'.floor($start+1))?>">></a>&nbsp;&nbsp;&nbsp;<a href="<?= base_url('index.php/admin/garmin/'.floor($activities/10))?>">>></a>
