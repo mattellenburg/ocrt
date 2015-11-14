97539959ea7dd78ca02415b39977c391af6d2daf
mattellenburg <mattellenburg@gmail.com>
2015-11-14 13:07:45 -0500

    Race
    Added race table

--- application/controllers/Race.php
+++ application/controllers/Race.php
@@ -8,15 +8,22 @@ class Race extends CI_Controller {
         $this->load->helper(array('url'));
         $this->load->helper('form');
         $this->load->library('form_validation');
+        $this->load->library('table');
         $this->load->model('race_model');
     }
 
     public function index() {
         $data = new stdClass();
         $data->message = '';
-        
+
+        $this->table->set_heading('Race', 'Date', 'Loacation', 'Description');
+
+        foreach($this->race_model->get_races() as $race) {
+            $this->table->add_row($race->race, $race->date, $race->location, $race-> description);
+        }
+
         $this->load->view('header', $data);
-        $this->load->view('race');
+        $this->load->view('race', $data);
         $this->load->view('footer');	
     }
 }
--- application/models/Race_model.php
+++ application/models/Race_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Roint_model extends CI_Model {
+class Race_model extends CI_Model {
     public function __construct() {		
         parent::__construct();
         $this->load->database();		
